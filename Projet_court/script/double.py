9ea7467ed0ef629784e2d1920f68106b6561227d
ragouradja <radja.ragou@gmail.com>
2021-09-01 20:45:14 +0200

    matrix distance added

--- Projet_court/script/double.py
+++ Projet_court/script/double.py
@@ -1,10 +1,57 @@
-def pdb2str(filename):
-	with open(filename, "r") as filin:
-		num = -1
-		for line in filin:
+from typing import ContextManager
+from Bio import SeqIO
+import numpy as np
+from numpy.core.numerictypes import maximum_sctype
+import pandas as pd
+from pandas.core.indexes.base import maybe_extract_name
+from scipy.spatial.distance import cdist
+import time
+
+def pdbtoseq(file):
+	with open(file, "r") as pdb:
+		for record in SeqIO.parse(pdb, 'pdb-atom'):
+			return record.seq
+
+def pdb2df(file):
+	content = []
+	with open(file,"r") as pdb:
+		for line in pdb:
 			if line.startswith("ATOM"):
 				items = line.split()
-				if num == -1:
-					num = items[5]
-				else:
-					
+				if items[2] == "CA":
+					content.append(items)
+	df = pd.DataFrame(content)
+	return df
+
+def get_coords(df):
+	return df[[6,7,8]].to_numpy(),(df[3]+df[5]).to_numpy()
+
+def matrix_distance(coords, colnames):
+	matrix = pd.DataFrame(cdist(coords, coords,metric="euclidean"))
+	matrix.columns = colnames
+	matrix.index = colnames
+	print(matrix)
+
+
+
+
+start = time.time()
+
+file = "prot.pdb"
+df = pdb2df(file)
+coords,colnames = get_coords(df)
+
+matrix_distance(coords,colnames)
+
+print(time.time() - start)
+#print(sequence)
+
+"""
+a = [[1,1,1],[2,1,2],[3,3,2]]
+b = [[1,1,1],[2,1,2],[3,3,2]]
+
+start = time.time()
+results = distance.cdist(a,b,metric="euclidean")
+print(results)
+print(time.time() - start)
+"""
